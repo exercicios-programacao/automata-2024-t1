515dd5e115ea809973d55d20936028f6a7ebf859
Ana Julia Ribeiro Pereira <ana.pereira@meta.com.br>
2024-04-26 10:53:12 -0300

    primeiro commit :)

--- src/automata.py
+++ src/automata.py
@@ -34,17 +34,53 @@ def load_automata(filename):
 
     """
 
-    with open(filename, "rt") as arquivo:
-        # processa arquivo...
-        pass
+
+arq = open("arquivo.txt")
+arquivo = arq.readlines()
+linha = arquivo.split("\n")
+
+estadoInicial = ""
+
+estadoInicial = linha[0]
+alfabeto = linha[1].split(" ")
+estados = linha[2].split(" ")
+estadosFinais = linha[3].split(" ")
+nodos = linha[4:]
+
+# print("Estado Inicial:", estadoInicial)
+# print("Alfabeto:", alfabeto)
+# print("Estados:", estados)
+# print("Estados Finais:", estadosFinais)
+# print("Nodos:", nodos)
+
+palavra = input("Informe a palavra: ")
+
+atual = estadoInicial
 
 
 def process(automata, words):
-    """
-    Processa a lista de palavras e retora o resultado.
+  for nodo in nodos:
+    n = nodo.split(" ")
+    if n[0] == automata and words == n[2]:
+      return n[1]
+  return None
     
-    Os resultados válidos são ACEITA, REJEITA, INVALIDA.
-    """
-
-    for word in words:
+i = -1
+for letra in palavra:
+  i += 1
+  atual = process(atual, letra)
+  if atual is None:
+    print("INVALIDA \n ")
+    break
+  else:
+    print(atual + ", " + letra)
+    if i != len(palavra) - 1:
+      print("Para")
+    else:
+      if atual in estadosFinais:
+        print("\n ACEITA ")
+      else:
+        print("\n REJEITA")
+        
+   # for word in words:
         # tenta reconhecer `word`
