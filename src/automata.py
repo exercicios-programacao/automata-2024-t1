072e52ad3c631e6b6e0e0a9c53998a6f39509950
Ana Julia <umanaribeiro@gmail.com>
2024-04-27 20:19:53 -0300

    ajustes de def e variaveis globais

--- src/automata.py
+++ src/automata.py
@@ -1,58 +1,46 @@
 """Implementação de autômatos finitos."""
-
-
+    
+   
 def load_automata(filename):
 
- with open(filename, "rt") as arquivo:
+    arq = open(filename, "rt")
     arquivo = arq.readlines()
     linha = arquivo.split("\n")
 
-    estadoInicial = ""
+    global estadoInicial
+    global alfabeto 
+    global estados 
+    global estadosFinais 
+    global nodos   
 
     estadoInicial = linha[0]
     alfabeto = linha[1].split(" ")
     estados = linha[2].split(" ")
     estadosFinais = linha[3].split(" ")
-    nodos = linha[4:]
-
-# print("Estado Inicial:", estadoInicial)
-# print("Alfabeto:", alfabeto)
-# print("Estados:", estados)
-# print("Estados Finais:", estadosFinais)
-# print("Nodos:", nodos)
-    pass
-
-
-
-# palavra = input("Informe a palavra: ")
-
-
+    nodos = linha[4:]        
 
 
 def process(automata, words):
-  for nodo in nodos:
-    n = nodo.split(" ")
-    if n[0] == automata and words == n[2]:
-      return n[1]
-  return None
+    for nodo in nodos:
+     n = nodo.split(" ")
+     if n[0] == automata and words == n[2]:
+        return n[1]
+    return None
 
-atual = estadoInicial    
-i = -1
-for letra in palavra:
-  i += 1
-  atual = process(atual, letra)
-  if atual is None:
-    print("INVALIDA \n ")
-    break
-  else:
-    print(atual + ", " + letra)
-    if i != len(palavra) - 1:
-      print("Para")
-    else:
-      if atual in estadosFinais:
-        print("\n ACEITA ")
-      else:
-        print("\n REJEITA")
-        
-   # for word in words:
-        # tenta reconhecer `word`
+
+    i = -1
+    for letra in palavra:
+        i += 1
+        atual = procuraEstados(atual, letra)
+        if atual is None:
+            print("INVALIDA \n ")
+            break
+        else:
+            print(atual + ", " + letra)
+            if i != len(palavra) - 1:
+                print("Para")
+            else:
+                if atual in estadosFinais:
+                    print("\n ACEITA ")
+                else:
+                    print("\n REJEITA")
