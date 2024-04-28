dc70a0093b9f0284f127376c5c9357a6cc7230bd
thiagokunrath <162643147+thiagokunrath@users.noreply.github.com>
2024-04-28 20:48:18 -0300

    Update automata.py

--- src/automata.py
+++ src/automata.py
@@ -1,50 +1,88 @@
-"""Implementação de autômatos finitos."""
-
-
 def load_automata(filename):
-    """
-    Lê os dados de um autômato finito a partir de um arquivo.
+  if isinstance(filename, str):
+      try:
+          with open(filename) as f:
+              lines = f.readlines()
+              if len(lines) < 4:
+                  raise Exception('File does not contain enough lines to represent an automata')
 
-    A estsrutura do arquivo deve ser:
+              symbols = tuple(lines[0].strip().split(' '))
+              states = tuple(lines[1].strip().split(' '))
 
-    <lista de símbolos do alfabeto, separados por espaço (' ')>
-    <lista de nomes de estados>
-    <lista de nomes de estados finais>
-    <nome do estado inicial>
-    <lista de regras de transição, com "origem símbolo destino">
+              final_states = tuple(state for state in lines[2].strip().split(' ') if state in states)
+              if len(final_states) == 0:
+                  raise Exception('Final states must be present in the automata description')
 
-    Um exemplo de arquivo válido é:
+              initial_state = lines[3].strip()
+              if initial_state not in states:
+                  raise Exception('The initial state is not present in the automata description')
 
-    ```
-    a b
-    q0 q1 q2 q3
-    q0 q3
-    q0
-    q0 a q1
-    q0 b q2
-    q1 a q0
-    q1 b q3
-    q2 a q3
-    q2 b q0
-    q3 a q1
-    q3 b q2
-    ```
+              rules = []
+              for line in lines[4:]:
+                  rule = tuple(line.strip().split(' '))
+                  if len(rule) < 3 or rule[0] not in states or rule[1] not in symbols or rule[2] not in states:
+                      raise Exception('Invalid transition rule')
+                  rules.append(rule)
 
-    Caso o arquivo seja inválido uma exceção Exception é gerada.
+              return {
+                  'symbols': symbols,
+                  'states': states,
+                  'final_states': final_states,
+                  'initial_state': initial_state,
+                  'rules': rules
+              }
+      except FileNotFoundError:
+          raise Exception('File not found in the system')
+  else:
+      raise Exception('Expected type for filename is string')
 
-    """
+def process(automata, word):
+  if isinstance(automata, dict) and isinstance(word, list):
+    for w in word:
+      if not isinstance(w, str):
+        raise Exception('As palavras devem ser do tipo string')
+  else:
+    raise Exception('O tipo esperado para o autômato é dict e para a palavra é list')
 
-    with open(filename, "rt") as arquivo:
-        # processa arquivo...
-        pass
+  try:
+    symbols = automata['symbols']
+    states = automata['states']
+    final_states = automata['final_states']
+    initial_state = automata['initial_state']
+    rules = automata['rules']
+  except KeyError:
+    raise Exception('O autômato não possui todos os campos esperados')
 
+  response = []
+  words = tuple(word)
 
-def process(automata, words):
-    """
-    Processa a lista de palavras e retora o resultado.
-    
-    Os resultados válidos são ACEITA, REJEITA, INVALIDA.
-    """
+  for word in words:
+    container = None
+    actual_state = initial_state
 
-    for word in words:
-        # tenta reconhecer `word`
+    for c in word:
+      if container:
+        break
+
+      if c not in symbols:
+        container = (word, 'INVALIDA')
+        response.append(container)
+        break
+
+      for rule in rules:
+        if rule[0] == actual_state and rule[1] == c:
+          actual_state = rule[2]
+          break
+      else:
+        container = (word, 'REJEITA')
+        response.append(container)
+        break
+    else:
+      if actual_state in final_states:
+        container = (word, 'ACEITA')
+        response.append(container)
+      else:
+        container = (word, 'REJEITA')
+        response.append(container)
+  else:
+    return response
