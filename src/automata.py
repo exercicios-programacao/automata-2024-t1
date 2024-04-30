a799cc21cc79c37eb81eba0cf869649698abce93
Ana Julia <umanaribeiro@gmail.com>
2024-04-30 11:09:01 -0300

    ajustes do PYlint

--- src/automata.py
+++ src/automata.py
@@ -1,38 +1,41 @@
-"""Implementação de autômatos finitos."""
-    
-   
+ESTADOINICIAL = ""
+ALFABETO  = ""
+ESTADO = ""
+ESTADOSFINAIS = ""
+NODOS  = ""
+
 def load_automata(filename):
+    """Chama arquivo e abre"""
+    with open(filename, encoding="utf-8") as arq:
+        arquivo = arq.readlines()
+        linha = arquivo.split("\n")
 
-    arq = open(filename, "rt")
-    arquivo = arq.readlines()
-    linha = arquivo.split("\n")
+        global estadoInicial
+        global alfabeto 
+        global estados 
+        global estadosFinais 
+        global nodos   
 
-    global estadoInicial
-    global alfabeto 
-    global estados 
-    global estadosFinais 
-    global nodos   
-
-    estadoInicial = linha[0]
-    alfabeto = linha[1].split(" ")
-    estados = linha[2].split(" ")
-    estadosFinais = linha[3].split(" ")
-    nodos = linha[4:]        
-
-def procuraEstado(automata, words):
-    for nodo in nodos:
-     n = nodo.split(" ")
-     if n[0] == automata and words == n[2]:
-        return n[1]
+        ESTADOINICIAL = linha[0]
+        ALFABETO = linha[1].split(" ")
+        ESTADO = linha[2].split(" ")
+        ESTADOSFINAIS = linha[3].split(" ")
+        nodos = linha[4:]        
+        
+def procura_estado(automata, words):
+    """procura estados"""
+    for nodo in NODOS:
+        n = nodo.split(" ")
+        if n[0] == automata and words == n[2]:
+            return n[1]
     return None
 
-
-
 def process(automata, words):
+    """Processo"""
     i = -1
     for letra in words:
         i += 1
-        atual =  procuraEstado(atual, letra)
+        atual =  procura_estado(atual, letra)
         if atual is None:
             print("INVALIDA \n ")
             break
@@ -41,7 +44,7 @@ def process(automata, words):
             if i != len(words) - 1:
                 print("Para")
             else:
-                if atual in estadosFinais:
+                if atual in ESTADOSFINAIS:
                     print("\n ACEITA ")
                 else:
-                    print("\n REJEITA")
+                    print("\n REJEITA")                   
