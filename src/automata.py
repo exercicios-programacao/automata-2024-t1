a273519113d98124dfa1ad68b004fcbd6d567b58
Danielle Parmigiani Werutsky <daniwerutsky@gmail.com>
2024-05-23 19:46:24 -0300

    alteracoes pylint, flake8, pydocstyle

--- src/automata.py
+++ src/automata.py
@@ -1,42 +1,74 @@
-def load_automata(filename: str) -> tuple:
-    with open(filename, "rt") as arquivo:
+"""Implementação de autômatos finitos."""
+
+
+def load_automata(filename: str):
+    """
+    Lê os dados de um autômato finito a partir de um arquivo.
+
+    A estsrutura do arquivo deve ser:
+
+    <lista de símbolos do alfabeto, separados por espaço (' ')>
+    <lista de nomes de estados>
+    <lista de nomes de estados finais>
+    <nome do estado inicial>
+    <lista de regras de transição, com "origem símbolo destino">
+
+    Um exemplo de arquivo válido é:
+
+    ```
+    a b
+    q0 q1 q2 q3
+    q0 q3
+    q0
+    q0 a q1
+    q0 b q2
+    q1 a q0
+    q1 b q3
+    q2 a q3
+    q2 b q0
+    q3 a q1
+    q3 b q2
+    ```
+
+    Caso o arquivo seja inválido uma exceção Exception é gerada.
+
+    """
+    with open(filename, "rt", encoding="utf-8") as arquivo:
         lines = arquivo.readlines()
-
         sigma = lines[0].strip().split()
-
-        Q = lines[1].strip().split()
-        F = lines[2].strip().split()
-        for estado in F:
-            if estado not in Q:
-                raise Exception("Estado final '{}' não está na lista de estados.".format(estado))
-
+        q = lines[1].strip().split()
+        f = lines[2].strip().split()
+        for estado in f:
+            if estado not in q:
+                raise ValueError("Estado final não está na lista de estados.")
         q0 = lines[3].strip()
-        if q0 not in Q:
-            raise Exception("O estado inicial não está na lista de estados.")
-
+        if q0 not in q:
+            raise ValueError("O estado inicial não está na lista de estados.")
         delta = {}
         for linha in lines[4:]:
             origem, simbolo, destino = linha.strip().split()
-            if origem not in Q or destino not in Q or simbolo not in sigma:
-                raise Exception("Transição inválida")
+            if origem not in q or destino not in q or simbolo not in sigma:
+                raise ValueError("Transição inválida")
             if origem not in delta:
                 delta[origem] = {}
             delta[origem][simbolo] = destino
+    return q, sigma, delta, q0, f
 
-    return Q, sigma, delta, q0, F
 
 def process(automata, words):
-    Q, sigma, delta, q0, F = automata
-    resultado = {}
+    """
+    Processa a lista de palavras e retora o resultado.
 
+    Os resultados válidos são ACEITA, REJEITA, INVALIDA.
+    """
+    sigma, delta, q0, f = automata
+    resultado = {}
     for word in words:
         letras = list(word)
-        
         for letra in letras:
             if letra not in sigma:
                 resultado[word] = 'INVALIDA'
                 break
-        
         else:
             atual = q0
             for letra in letras:
@@ -46,9 +78,8 @@ def process(automata, words):
                     resultado[word] = 'REJEITA'
                     break
             else:
-                if atual in F:
+                if atual in f:
                     resultado[word] = 'ACEITA'
                 else:
                     resultado[word] = 'REJEITA'
-                
     return resultado
