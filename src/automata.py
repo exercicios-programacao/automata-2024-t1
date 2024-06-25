8abbbb3888f5713bd5632bc69c2bd079e3ca0838
eduardassalim <167265171+eduardassalim@users.noreply.github.com>
2024-06-25 20:30:34 -0300

    Update automata.py

--- src/automata.py
+++ src/automata.py
@@ -5,10 +5,12 @@ class ErroException(Exception):
     """Aqui crio um exceção personalizada.
     atributos:
         mensagem (str): descrição do erro encontrado."""
+
     def __init__(self, mensagem):
         """Aqui inicializa a exceção.
         Args:
             mensagem (str): Mensagem do erro."""
+
         self.mensagem = mensagem
         super().__init__(self.mensagem)
 
