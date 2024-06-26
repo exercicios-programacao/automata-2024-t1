9763c9e9445273515254de488e73d56a8f470894
eduardassalim <167265171+eduardassalim@users.noreply.github.com>
2024-06-25 21:28:10 -0300

    Update automata.py

--- src/automata.py
+++ src/automata.py
@@ -2,24 +2,22 @@
 
 
 class ErroException(Exception):
-    """Aqui crio uma exceção personalizada.
-
-    Args:
-        mensagem (str): descrição do erro encontrado.
-    """
+    """Aqui crio um exceção personalizada.
+    atributos:
+        mensagem (str): descrição do erro encontrado."""
 
     def __init__(self, mensagem):
         """Aqui inicializa a exceção.
-
         Args:
-            mensagem (str): Mensagem do erro.
-        """
+            mensagem (str): Mensagem do erro."""
+
         self.mensagem = mensagem
         super().__init__(self.mensagem)
 
 
 def load_automata(filename: str):
     """Aqui carrega um autômato a partir de um arquivo."""
+
     try:
         with open(filename, encoding='utf-8') as arquivo:
             linhas = arquivo.readlines()
@@ -36,12 +34,10 @@ def load_automata(filename: str):
 
             for linha in linhas[4:]:
                 transicao = linha.strip().split()
-                if (
-                    len(transicao) != 3 and
-                    transicao[0] not in estados and
-                    transicao[1] not in alfabeto and
-                    transicao[2] not in estados
-                ):
+                if (len(transicao) != 3
+                        or transicao[0] not in estados
+                        or transicao[1] not in alfabeto
+                        or transicao[2] not in estados):
                     raise ErroException("Transição inválida.")
 
                 estado_origem = transicao[0]
@@ -61,7 +57,9 @@ def load_automata(filename: str):
 
 def process(automata, words):
     """Aqui processa lista de palavras."""
+
     alfabeto, estados_finais, estado_inicial, transicoes = automata
+
     verifica = {}
     try:
         for word in words:
