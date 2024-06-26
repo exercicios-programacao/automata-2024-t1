513e9476e80f0b8790f8759b8041a3862f8d73d4
eduardassalim <167265171+eduardassalim@users.noreply.github.com>
2024-06-25 21:46:34 -0300

    Update automata.py

--- src/automata.py
+++ src/automata.py
@@ -36,9 +36,9 @@ def load_automata(filename: str):
 
             for linha in linhas[4:]:
                 transicao = linha.strip().split()
-                if (len(transicao) != 3 and
-                        transicao[0] not in estados and
-                        transicao[1] not in alfabeto and
+                if (len(transicao) != 3 or
+                        transicao[0] not in estados or
+                        transicao[1] not in alfabeto or
                         transicao[2] not in estados):
                     raise ErroException("Transição inválida.")
 
